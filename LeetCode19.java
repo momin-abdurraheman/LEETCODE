598b4a2c86755ef4b664d1bd26c2c303e105b2a2
momin-abdurraheman <111493298+momin-abdurraheman@users.noreply.github.com>
2023-07-10 12:40:57 +0530

    Create LeetCode19.java

---
+++ LeetCode19.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode current= dummy;
+        ListNode nth = dummy;
+        for(int i=1;i<=n+1;i++){
+            current = current.next;
+        }
+        while(current!=null){
+            current = current.next;
+            nth = nth.next;
+        }
+        // cutting the link
+        nth.next = nth.next.next;
+        return dummy.next;
+    }
+}
